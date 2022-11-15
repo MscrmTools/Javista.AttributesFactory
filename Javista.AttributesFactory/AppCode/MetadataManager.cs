dbc741dff076c60aed19096a5ec9a3a486449baf
MscrmTools <tanguy92@hotmail.com>
2022-11-15 16:42:55 +0100

    Retrieve all tables without filter when default solution has been selected

--- Javista.AttributesFactory/AppCode/MetadataManager.cs
+++ Javista.AttributesFactory/AppCode/MetadataManager.cs
@@ -13,52 +13,53 @@ namespace Javista.AttributesFactory.AppCode
     {
         public static List<EntityMetadata> GetEntities(Guid solutionid, IOrganizationService service)
         {
-            var components = service.RetrieveMultiple(new QueryExpression("solutioncomponent")
+            List<Guid> tablesIds = new List<Guid>();
+
+            // If solution is not default solution
+            if (!solutionid.Equals(new Guid("fd140aaf-4df4-11dd-bd17-0019b9312238")))
             {
-                ColumnSet = new ColumnSet("objectid"),
-                NoLock = true,
-                Criteria = new FilterExpression
+                var components = service.RetrieveMultiple(new QueryExpression("solutioncomponent")
                 {
-                    Conditions =
+                    ColumnSet = new ColumnSet("objectid"),
+                    NoLock = true,
+                    Criteria = new FilterExpression
+                    {
+                        Conditions =
                         {
                             new ConditionExpression("solutionid", ConditionOperator.Equal,solutionid),
                             new ConditionExpression("componenttype", ConditionOperator.Equal, 1)
                         }
-                }
-            }).Entities;
-
-            var list = components.Select(component => component.GetAttributeValue<Guid>("objectid"))
-                .ToList();
-
-            if (list.Count > 0)
-            {
-                EntityQueryExpression entityQueryExpression = new EntityQueryExpression
-                {
-                    Criteria = new MetadataFilterExpression(LogicalOperator.Or),
-                    Properties = new MetadataPropertiesExpression
-                    {
-                        AllProperties = false,
-                        PropertyNames = { "DisplayName", "LogicalName" }
                     }
-                };
+                }).Entities;
 
-                list.ForEach(id =>
+                tablesIds = components.Select(component => component.GetAttributeValue<Guid>("objectid"))
+                    .ToList();
+            }
+
+            EntityQueryExpression entityQueryExpression = new EntityQueryExpression
+            {
+                Criteria = new MetadataFilterExpression(LogicalOperator.Or),
+                Properties = new MetadataPropertiesExpression
+                {
+                    AllProperties = false,
+                    PropertyNames = { "DisplayName", "LogicalName" }
+                }
+            };
+
+            tablesIds.ForEach(id =>
                 {
                     entityQueryExpression.Criteria.Conditions.Add(new MetadataConditionExpression("MetadataId", MetadataConditionOperator.Equals, id));
                 });
 
-                RetrieveMetadataChangesRequest retrieveMetadataChangesRequest = new RetrieveMetadataChangesRequest
-                {
-                    Query = entityQueryExpression,
-                    ClientVersionStamp = null
-                };
+            RetrieveMetadataChangesRequest retrieveMetadataChangesRequest = new RetrieveMetadataChangesRequest
+            {
+                Query = entityQueryExpression,
+                ClientVersionStamp = null
+            };
 
-                var response = (RetrieveMetadataChangesResponse)service.Execute(retrieveMetadataChangesRequest);
+            var response = (RetrieveMetadataChangesResponse)service.Execute(retrieveMetadataChangesRequest);
 
-                return response.EntityMetadata.ToList();
-            }
-
-            return new List<EntityMetadata>();
+            return response.EntityMetadata.ToList();
         }
 
         public static List<string> GetNotExistingEntities(List<string> entities, IOrganizationService service)
